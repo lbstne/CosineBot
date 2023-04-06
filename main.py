81bb5cc073a84ee27c57c30be3804b39998a7ede
Aiden Fox Ivey <aidenfoxivey@disroot.org>
2023-04-06 00:01:01 -0400

    Remove .idea and add error handling

--- main.py
+++ main.py
@@ -47,7 +47,8 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if client.user in message.mentions:
+    # prevent it from replying to itself
+    if client.user in message.mentions and message.author.id is not client.user:
 
         # replace [[url]] with actual content from web page
         await message.channel.typing()
@@ -66,8 +67,41 @@ async def on_message(message):
             user_message_histories[message.author.id].append({"role": "user", "content": formatted_message})
             message_to_send = system_prompts + user_message_histories[message.author.id]
 
-        completion = openai.ChatCompletion.create(model='gpt-4', messages=message_to_send)
-        msg = completion.choices[0].message.content
+        msg = ""
+
+        try:
+            completion = openai.ChatCompletion.create(model='gpt-4', messages=message_to_send)
+            msg = completion.choices[0].message.content
+
+        # error handling on API side
+        except openai.error.APIError as e:
+            msg = f"I'm sorry {message.author.id}, I can't do that for you."
+            print(f"API had an error: {e}")
+
+        except openai.error.Timeout as e:
+            msg = f"I'm sorry {message.author.id}. I timed out. Maybe try again?"
+            print(f"Request timeout: {e}")
+
+        except openai.error.RateLimitError as e:
+            msg = "Wow, you're a fast bugger aren't you? Chill on the requests for a second."
+            print("Rate limit hit.")
+
+        except openai.error.APIConnectionError as e:
+            msg = f"I'm sorry {message.author.id}, I can't do that for you."
+            print(f"API had an error: {e}")
+
+        except openai.error.InvalidRequestError as e:
+            msg = "Whoever wrote this bot did a shitty job, so tell them to fix it."
+            print(f"Invalid request: {e}")
+
+        except openai.error.AuthenticationError as e:
+            msg = "Luca's token failed. LMAO RIP BOZO"
+            print(f"Failed auth: {e}")
+
+        except openai.error.ServiceUnavailableError as e:
+            msg = "Question for you. What's better than octopus recipe? Answer for you: 8 recipes for octopus. My grandmother give me a family recipe before she died in a horrible way."
+            print(f"Check OpenAI's status page: {e}")
+
         await message.reply(msg)
 
 client.run(os.environ['DISCORD_API_KEY'])
